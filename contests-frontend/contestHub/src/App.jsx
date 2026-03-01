78e8719d84ba63fe802e1a058f7682417864486d
Anshuman <chaoanxuman999@gmail.com>
2026-03-02 02:11:52 +0530

    shadcn-ui

--- contests-frontend/contestHub/src/App.jsx
+++ contests-frontend/contestHub/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
+     <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Is this a button?</Button>
+    </div>
   )
 }
 
